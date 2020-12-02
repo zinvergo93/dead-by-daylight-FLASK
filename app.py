a94f3e8a31da68656796f1f241c07b01ed4d240d
Zachary Invergo <zinvergo@gmail.com>
2020-12-02 12:58:38 -0600

    added line to route database to public server in order to populate

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_heroku import Heroku
 import os
 
-
 app = Flask(__name__)
+heroku = Heroku(app)
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
-    basedir, 'app.sqlite'
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+#     basedir, 'app.sqlite'
+# )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
